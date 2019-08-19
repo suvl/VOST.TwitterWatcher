eb1336860196d008b991381ade63a57b06b7d0a6
João Trigo Soares <joao@jtsoar.es>
2019-08-19 13:18:40 +0100

    added nlog

--- VOST.TwitterWatcher/Program.cs
+++ VOST.TwitterWatcher/Program.cs
@@ -1,12 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace VOST.TwitterWatcher
 {
@@ -21,7 +17,21 @@ namespace VOST.TwitterWatcher
         /// <param name="args">the program's arguments.</param>
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config")
+                .GetCurrentClassLogger();
+            try
+            {
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, "Stopping program execution");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         /// <summary>
@@ -31,6 +41,12 @@ namespace VOST.TwitterWatcher
         /// <returns>An <see cref="IWebHostBuilder" />.</returns>
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureLogging(l =>
+                {
+                    l.ClearProviders();
+                    l.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
